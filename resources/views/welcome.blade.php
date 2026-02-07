8ee91594838b7419388b405350e5044ef0756819
murat pektas <murat@MacBook-Air.local>
2026-02-07 22:28:28 +0300

    Fix favicon and touch icon paths for production public directory

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,9 @@
 
         <title>Laravel</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/public/favicon.ico" sizes="any">
+        <link rel="icon" href="/public/favicon.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/public/apple-touch-icon.png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
